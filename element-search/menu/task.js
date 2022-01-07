df225a8ef22b6b8bf5f37328d97c12d4aaff42db
Dmitry Sheshukov <yooyklon@gmail.com>
2022-01-07 16:17:37 +0700

    Added the homeMenu function to the element-search/menu/task.js file

---
+++ element-search/menu/task.js
@@ -0,0 +1,26 @@
+function homeMenu() {
+ let menuLinks = document.querySelectorAll('.menu__link');
+ menuLinks = Array.from(menuLinks);
+ 
+ for (let i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].onclick = function() {
+   if (menuLinks[i].nextElementSibling) {
+    let menuSub = menuLinks[i].nextElementSibling;
+ 
+    if (menuSub.className.includes('menu_active')) {
+     menuSub.className = 'menu menu_sub';
+     return false;
+    } else {
+      let menuSub = menuLinks[i].nextElementSibling;
+ 
+       menuSub.className += ' menu_active';
+       return false;
+    }
+   }
+  }
+ }
+}
+
+homeMenu();
+
+
