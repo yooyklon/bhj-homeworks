be4c35307033b6ac8401168cc624d5a899d62e31
Dmitry Sheshukov <yooyklon@gmail.com>
2022-03-26 23:49:29 +0700

    fixed dom/ads/task.js file

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,10 +1,14 @@
 let rotatorCaseActive = document.querySelectorAll('.rotator__case')[0];
 let newRotatorCaseActive = null;
+let rotatorCaseColor = document.querySelectorAll('.rotator__case');
+
+rotatorCaseColor.forEach(elem => {
+ elem.style.color = elem.dataset.color;
+})
 
 setTimeout(function rotator() {
  rotatorCaseActive.style.color = rotatorCaseActive.dataset.color;
  
-
  newRotatorCaseActive = rotatorCaseActive.nextElementSibling || document.querySelectorAll('.rotator__case')[0];
  rotatorCaseActive.classList.remove('rotator__case_active');
  newRotatorCaseActive.classList.add('rotator__case_active');
