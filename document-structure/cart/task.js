0db89f4ecf7e9a91fdb95d9d31755c8c59a5194c
Dmitry Sheshukov <yooyklon@gmail.com>
2022-03-28 14:03:37 +0700

    fixed document-structure/cart/task.js file

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,5 +1,5 @@
 let products = document.querySelector('.products');
-let cartProducts = document.querySelector('.cart__products');
+let cartProducts = Array.from(document.getElementsByClassName('cart__products'))[0];
 
 products.addEventListener('click', function(event) {
  let value = event.target.closest('.product').querySelector('.product__quantity-value');
@@ -11,16 +11,17 @@ products.addEventListener('click', function(event) {
    value.innerText = Number.parseInt(value.innerText) + 1;
   }
  } 
+
  if (event.target.closest('.product__add')) {
   let basket = Array.from(document.querySelectorAll('.cart__product'));
   let cart = event.target.closest('.product');
   let productId = cart.dataset.id;
   let productImg = cart.querySelector('.product__image').src;
   let productCount = cart.querySelector('.product__quantity-value').innerText;
+  let productElement = basket.find(elem => elem.dataset.id == productId);
 
-  if (basket.some(elem => elem.dataset.id == productId)) {
-   let elem = basket.find(elem => elem.dataset.id == productId);
-   elem.querySelector('.cart__product-count').innerText = Number(elem.querySelector('.cart__product-count').innerText) + +productCount;
+  if (productElement) {
+    productElement.querySelector('.cart__product-count').innerText = Number(productElement.querySelector('.cart__product-count').innerText) + +productCount;
   } else {
    cartProducts.insertAdjacentHTML(
     'afterbegin',
