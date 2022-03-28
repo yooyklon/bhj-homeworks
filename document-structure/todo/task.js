06cfe56458d989470f1a052b6074349a801ddf65
Dmitry Sheshukov <yooyklon@gmail.com>
2022-03-28 12:37:52 +0700

    fixed document-structure/todo/task.js file

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,19 +4,19 @@ let taskLists = document.querySelector('.tasks__list');
 
 formSubmite.addEventListener('click', function(event) {
  event.preventDefault();
- if (!inputForm == '') {
-  let text = inputForm.value;
-  taskLists.insertAdjacentHTML(
-   'afterbegin',
-   `<div class="task">
-   <div class="task__title">
-   ${text}
-   </div>
-   <a href="#" class="task__remove">&times;</a>
-   </div>`
-   )
-  inputForm.value = '';
- }
+  let text = inputForm.value.trim();
+  if (text != '') {
+   taskLists.insertAdjacentHTML(
+    'afterbegin',
+    `<div class="task">
+    <div class="task__title">
+    ${text}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+    </div>`
+    )
+   inputForm.value = '';
+  }
 })
 
 taskLists.addEventListener('click', function(event) {
