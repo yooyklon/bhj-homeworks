894832d98f165d26fdece8368b62ddf5b48e0dfa
Dmitry Sheshukov <yooyklon@gmail.com>
2022-03-28 14:05:26 +0700

    fixed document-structure/tooltip/task.js file

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,15 +4,15 @@ document.addEventListener('click', function(event) {
  event.preventDefault();
  let positionTarget;
  let positionTooltip;
- 
- if (event.target.closest('.has-tooltip')) {
-  tooltip.classList.toggle('tooltip_active');
-  
-  positionTarget = event.target.getBoundingClientRect();
-  tooltip.innerText = event.target.title;
-  positionTooltip = tooltip.getBoundingClientRect();
 
-  switch(tooltip.dataset.position) {
+ if (event.target.closest('.has-tooltip')) { 
+ tooltip.classList.toggle('tooltip_active');
+  
+ positionTarget = event.target.getBoundingClientRect();
+ tooltip.innerText = event.target.title;
+ positionTooltip = tooltip.getBoundingClientRect();
+
+  switch(event.target.dataset.position) {
    case 'bottom': 
     tooltip.style.top = positionTarget.height + positionTarget.top + 'px';
     tooltip.style.left = positionTarget.left + 'px';
@@ -28,6 +28,10 @@ document.addEventListener('click', function(event) {
    case 'left':
     tooltip.style.top = positionTarget.top  + 'px';
     tooltip.style.left = positionTarget.left - positionTooltip.width + 'px';
+    break;
+   default: 
+    tooltip.style.top = positionTarget.height + positionTarget.top + 'px';
+    tooltip.style.left = positionTarget.left + 'px';
     break;
   }
  } else {
