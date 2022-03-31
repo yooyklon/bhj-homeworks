482741e60ec461e0a4bdbd3f615c34588e8e19ac
Dmitry Sheshukov <yooyklon@gmail.com>
2022-03-31 11:40:08 +0700

    added html-forms/autocomplete/task.js file

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,11 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    let value = text;
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    let options = Array.from(this.input);
+    let array = options.filter(element => element.text.includes(value));
+    return array.map(elem => ({text: elem.text, value: elem.value}));
   }
 }
 
