a07b59f6f0c83e7bec615b25e16af1a5e595765a
Dmitry Sheshukov <yooyklon@gmail.com>
2022-03-31 13:37:55 +0700

    fixed client-state/text-editor/task.js file

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,7 @@
 const editor = document.getElementById('editor');
 
 document.addEventListener("DOMContentLoaded", function() {
- if (editor.value === '') {
   editor.value = localStorage.getItem('editorValue');
- }
 
  editor.addEventListener('input', function(event) {
   let value = event.target.value;
